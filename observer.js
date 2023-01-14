1ad17a1ab473b944ce7cf30a5ebbfae96537b538
NPotokin <npotokin@gmail.com>
2023-01-14 11:57:50 -0700

    used custom classes to achieve animation

--- observer.js
+++ observer.js
@@ -1,9 +1,14 @@
+let observants = document.querySelectorAll(".scrolly")
+
+
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        const intersecting = entry.isIntersecting
-        entry.target.classList.add = intersecting ? "bg-slate-500 " : "opacity-0"
+        entry.target.classList.toggle("isShown", entry.isIntersecting);
     }),
     {threshold: 0.5}
+    console.log(entries);
 })
 
-observer.observe(document.getElementsByClassName("scrolly"));
+observants.forEach(observant => {
+    observer.observe(observant)
+});
